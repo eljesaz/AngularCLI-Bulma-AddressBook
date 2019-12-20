a244b9ad2ccc4177795825dda08f8c0130499cc4
Eljesa Zekjiri <Eljesa.Zekjiri@endava.com>
2019-12-20 10:37:28 +0100

    migrated from css to sass, added modals from add and delete person

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,20 +6,38 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
   person: IPerson;
   registerForm: FormGroup;
   submitted = false;
+  isSuccessModalActive = false;
+  isFailureModalActive = false;
 
   constructor(private service: ServiceService, private formBuilder: FormBuilder) {
     this.person = { name: null, email: null, message: null, _id: null };
   }
+
+  toggleSuccessModal() {
+    this.isSuccessModalActive = !this.isSuccessModalActive;
+  }
+  toggleFailureModal() {
+    this.isFailureModalActive = !this.isFailureModalActive;
+  }
+
   processForm() {
-    const allInfo = `My name is ${this.person.name}. My email is ${this.person.email}. My message is ${this.person.message}`;
-    alert(allInfo);
-    this.service.postPerson(this.person).subscribe();
+    if (this.person.name != "" && this.person.email !="") {
+      this.service.postPerson(this.person).subscribe();
+      this.isSuccessModalActive = true;
+
+      this.person.name = "";
+      this.person.email = "";
+      this.person.message = "";
+    }
+    else {
+      this.isFailureModalActive = true;
+    }
   }
 
   ngOnInit() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { IPerson } from '../../interfaces/IPerson';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   people: IPerson[];
